cd850b9cb31305667724e8bff09ff1664eb6923c
Thiago Ciavolela <44538895+thiagociavolela@users.noreply.github.com>
2025-03-27 18:02:06 +0000

    implementação desafio Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,71 +1,65 @@
 #include <stdio.h>
 
 int main() {
-    // Definição do tamanho do tabuleiro
     int tamanho_tabuleiro = 10;
     int tamanho_navio_vertical = 3;
     int tamanho_navio_horizontal = 4;
     int tamanho_navio_diagonal1 = 3;
     int tamanho_navio_diagonal2 = 4;
-    
-    // Definição do tabuleiro
-    int tabuleiro[10][10];
-    
-    // Inicializando o tabuleiro com 0
+
+    int tabuleiro[tamanho_tabuleiro][tamanho_tabuleiro];
     for (int i = 0; i < tamanho_tabuleiro; i++) {
         for (int j = 0; j < tamanho_tabuleiro; j++) {
             tabuleiro[i][j] = 0;
         }
     }
     
-    // Posição inicial dos navios
     int x_vertical = 2, y_vertical = 5;
     int x_horizontal = 7, y_horizontal = 1;
     int x_diagonal1 = 1, y_diagonal1 = 1;
     int x_diagonal2 = 6, y_diagonal2 = 6;
     
-    // Posicionando o navio vertical
     for (int i = 0; i < tamanho_navio_vertical; i++) {
         tabuleiro[x_vertical + i][y_vertical] = 3;
     }
     
-    // Posicionando o navio horizontal
     for (int i = 0; i < tamanho_navio_horizontal; i++) {
         tabuleiro[x_horizontal][y_horizontal + i] = 3;
     }
     
-    // Posicionando o navio diagonal 1
     for (int i = 0; i < tamanho_navio_diagonal1; i++) {
         tabuleiro[x_diagonal1 + i][y_diagonal1 + i] = 3;
     }
     
-    // Posicionando o navio diagonal 2
     for (int i = 0; i < tamanho_navio_diagonal2; i++) {
         tabuleiro[x_diagonal2 + i][y_diagonal2 - i] = 3;
     }
     
-    // Exibição das coordenadas dos navios
-    printf("Coordenadas do Navio Vertical:\n");
-    for (int i = 0; i < tamanho_navio_vertical; i++) {
-        printf("(%d, %d)\n", x_vertical + i, y_vertical);
-    }
+    // Aplicação de habilidades especiais
+    int habilidade_x = 4, habilidade_y = 4;
     
-    printf("\nCoordenadas do Navio Horizontal:\n");
-    for (int i = 0; i < tamanho_navio_horizontal; i++) {
-        printf("(%d, %d)\n", x_horizontal, y_horizontal + i);
-    }
+    // Cone
+    tabuleiro[habilidade_x - 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x][habilidade_y - 1] = 1;
+    tabuleiro[habilidade_x][habilidade_y] = 1;
+    tabuleiro[habilidade_x][habilidade_y + 1] = 1;
+    tabuleiro[habilidade_x + 1][habilidade_y - 1] = 1;
+    tabuleiro[habilidade_x + 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x + 1][habilidade_y + 1] = 1;
     
-    printf("\nCoordenadas do Navio Diagonal 1:\n");
-    for (int i = 0; i < tamanho_navio_diagonal1; i++) {
-        printf("(%d, %d)\n", x_diagonal1 + i, y_diagonal1 + i);
-    }
+    // Cruz
+    tabuleiro[habilidade_x - 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x + 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x][habilidade_y - 1] = 1;
+    tabuleiro[habilidade_x][habilidade_y + 1] = 1;
     
-    printf("\nCoordenadas do Navio Diagonal 2:\n");
-    for (int i = 0; i < tamanho_navio_diagonal2; i++) {
-        printf("(%d, %d)\n", x_diagonal2 + i, y_diagonal2 - i);
-    }
+    // Octaedro
+    tabuleiro[habilidade_x - 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x + 1][habilidade_y] = 1;
+    tabuleiro[habilidade_x][habilidade_y - 1] = 1;
+    tabuleiro[habilidade_x][habilidade_y + 1] = 1;
+    tabuleiro[habilidade_x][habilidade_y] = 1;
     
-    // Exibir tabuleiro
     printf("\nTabuleiro:\n");
     for (int i = 0; i < tamanho_tabuleiro; i++) {
         for (int j = 0; j < tamanho_tabuleiro; j++) {
